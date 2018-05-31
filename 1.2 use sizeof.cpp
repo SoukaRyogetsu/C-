84be578dfe83a9d49ea2cac9cfa622e4ebbc10bf
ichinichi <254875077@qq.com>
2018-06-01 08:30:54 +0900

    Update 1.2 use sizeof.cpp

--- 1.2 use sizeof.cpp
+++ 1.2 use sizeof.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"int: "<<sizeof(int)<<" bytes"<<endl;
-    cout<<"short: "<<sizeof(short)<<" bytes"<<endl;
-    cout<<"long int: "<<sizeof(long int)<<" bytes"<<endl;
-    cout<<"long long: "<<sizeof(long long)<<" bytes"<<endl;
-    cout<<"char: "<<sizeof(char)<<" byte"<<endl;
-    cout<<"bool: "<<sizeof(bool)<<" byte"<<endl;
-    cout<<"float: "<<sizeof(float)<<" bytes"<<endl;
-    cout<<"double: "<<sizeof(double)<<" bytes"<<endl;
-    cout<<"long double: "<<sizeof(long double)<<" bytes"<<endl;
+    cout<<"int: "<<sizeof(int)<<" bytes"<<endl;  //4
+    cout<<"short: "<<sizeof(short)<<" bytes"<<endl;  //2
+    cout<<"long int: "<<sizeof(long int)<<" bytes"<<endl;  //4
+    cout<<"long long: "<<sizeof(long long)<<" bytes"<<endl;   //8
+    cout<<"char: "<<sizeof(char)<<" byte"<<endl;  //1
+    cout<<"bool: "<<sizeof(bool)<<" byte"<<endl;  //1
+    cout<<"float: "<<sizeof(float)<<" bytes"<<endl;  //4
+    cout<<"double: "<<sizeof(double)<<" bytes"<<endl;  //8
+    cout<<"long double: "<<sizeof(long double)<<" bytes"<<endl;  //12
     return 0;
 }
