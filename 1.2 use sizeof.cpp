907ad54cf6f556bef86769a8983652c6ed45060a
ichinichi <254875077@qq.com>
2018-06-01 00:16:00 +0900

    create 1.2 use sizeof.cpp
    编程输出常用类型的字节长度

---
+++ 1.2 use sizeof.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"int: "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"short: "<<sizeof(short)<<" bytes"<<endl;
+    cout<<"long int: "<<sizeof(long int)<<" bytes"<<endl;
+    cout<<"long long: "<<sizeof(long long)<<" bytes"<<endl;
+    cout<<"char: "<<sizeof(char)<<" byte"<<endl;
+    cout<<"bool: "<<sizeof(bool)<<" byte"<<endl;
+    cout<<"float: "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"double: "<<sizeof(double)<<" bytes"<<endl;
+    cout<<"long double: "<<sizeof(long double)<<" bytes"<<endl;
+    return 0;
+}
